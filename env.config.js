2b92010c7241c62ab4051c0fdc5ee11e5b2499ed
Paulo Barcelos <paulo@azucrina.org>
2017-07-27 15:18:53 +0200

    Added env.config to repo

---
+++ env.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	STATIC         : process.env.STATIC ? true : false,
+	CACHE_SERVER   : 'http://localhost:4000',
+	GRAPHQL_SERVER : 'https://strawbees-content-graphql-server-jzfdsyevbr.now.sh'
+}
