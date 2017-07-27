6152a0c0ef8eb5f2d65c854dc4e23a7cef2e4d6e
Paulo Barcelos <paulo@azucrina.org>
2017-07-27 15:22:34 +0200

    Refactored hero

--- components/hero.js
+++ components/hero.js
@@ -1,16 +1,51 @@
+import generateClassnames from 'src/utils/generateClassnames'
+
 export default ({
 	children,
 	icon,
 	title,
-	description
+	subtitle,
+	description,
+	color
 }) =>
-<div className='hero'>
-	<img className='icon' src={icon.url} />
-	<h1 className='title'>
-		{title}
-	</h1>
-	<p className='description'>
-		{description}
-	</p>
+<div
+	className={`root hero ${generateClassnames({
+		children,
+		icon,
+		title,
+		subtitle,
+		description,
+		color
+	})}`}
+	style={{
+		backgroundColor : color
+	}}>
+	<style jsx>{`
+		.root.not-color {
+			background-color: #DDD;
+			color: #000;
+		}
+		.root.color {
+			color: #FFF;
+		}
+	`}</style>
+	{icon &&
+		<img className='icon' src={icon.url} />
+	}
+	{title &&
+		<h1 className='title'>
+			{title}
+		</h1>
+	}
+	{subtitle &&
+		<h2 className='subtitle'>
+			{subtitle}
+		</h2>
+	}
+	{description &&
+		<p className='description'>
+			{description}
+		</p>
+	}
 	{children}
 </div>
