20111c33ccb23ebb9e439b27b4060adf9d470b73
Paulo Barcelos <paulo@azucrina.org>
2017-07-27 15:21:53 +0200

    Refactored hoc getInitialProps

--- components/hoc/app.js
+++ components/hoc/app.js
@@ -6,55 +6,91 @@ import fetchLocalData from 'src/utils/fetchLocalData'
 
 export default Child => class App extends React.Component {
 	static async getInitialProps(ctx) {
-		const result = await Promise.all([
-			(async () => {
-				const {	locale } = ctx.query
-				const localData = await fetchLocalData(locale, `{
-					siteMetas(q: "order=-sys.createdAt&limit=1"){
-						locale
-						languageName
-						basename
-						avaiableLocales
+		// Retrieve props that will be used app wise
+		const {	locale } = ctx.query
+		const localData = await fetchLocalData(locale, `{
+			siteMetas(q: "order=-sys.createdAt&limit=1"){
+				locale
+				languageName
+				basename
+				avaiableLocales
 
-						lessonPlanSlug
-						lessonPlanCollectionSlug
-						lessonPlanGroupSlug
-					}
-				}`)
-				const data = localData.siteMetas.shift()
-				const currentLocale = {
-					locale       : data.locale,
-					languageName : data.languageName,
-					basename     : data.basename ? data.basename : ''
-				}
-				const avaiableLocales = data.avaiableLocales.map(line => {
-					const array = line.split('_')
-					return {
-						locale       : array[0],
-						languageName : array[1],
-						basename     : array[2] ? array[2] : ''
-					}
-				})
-				const contentTypeSlugs = Object.keys(data)
-					.filter(key => key.endsWith('Slug'))
-					.reduce((acc, key) => {
-						const type = key.replace('Slug', '')
-						acc[type] = data[key]
-						return acc
-					}, {})
-				return {
-					currentLocale,
-					avaiableLocales,
-					contentTypeSlugs
-				}
-			})(),
-			Child.getInitialProps(ctx, fetchLocalData)
-		])
-		const meta = result[0]
-		const props = result[1]
+				lessonPlanSlug
+				lessonPlanCollectionSlug
+				lessonPlanGroupSlug
+			}
+
+			textStrings(q: "order=-sys.createdAt&limit=1"){
+				content
+				home
+				searchLessonPlans
+				more
+				author
+				duration
+				classSize
+				groupSize
+				overview
+				materials
+				shop
+				modifications
+				learningObjectives
+				nationalStandards
+				teachingAssessment
+				preparation
+				lessonSteps
+				vocabulary
+				attachments
+				saveAsPrintableFile
+				download
+				relatedLessons
+				nextLesson
+				all
+				previousLesson
+				foundLessons
+				noLessonsFound
+				ageGroup
+				coMaterial
+				tag
+				lessonPlan
+				lessonPlanCollection
+				companyAddress
+				copyrightNotice
+			}
+		}`)
+		const siteMeta = localData.siteMetas.shift()
+		const strings = localData.textStrings.shift()
+		const currentLocale = {
+			locale       : siteMeta.locale,
+			languageName : siteMeta.languageName,
+			basename     : siteMeta.basename ? siteMeta.basename : ''
+		}
+		const avaiableLocales = siteMeta.avaiableLocales.map(line => {
+			const array = line.split('_')
+			return {
+				locale       : array[0],
+				languageName : array[1],
+				basename     : array[2] ? array[2] : ''
+			}
+		})
+		const contentTypeSlugs = Object.keys(siteMeta)
+			.filter(key => key.endsWith('Slug'))
+			.reduce((acc, key) => {
+				const type = key.replace('Slug', '')
+				acc[type] = siteMeta[key]
+				return acc
+			}, {})
+
+		const appProps = {
+			currentLocale,
+			avaiableLocales,
+			contentTypeSlugs,
+			strings
+		}
+		// Retrive props of the current page
+		const pageProps = await Child.getInitialProps(ctx, fetchLocalData, appProps)
 		return {
-			meta,
-			...props
+			appProps,
+			...pageProps
 		}
 	}
 
@@ -62,7 +98,7 @@ export default Child => class App extends React.Component {
 		return (
 			<div>
 				<Head></Head>
-				<LocalesMenu {...this.props.meta}/>
+				<LocalesMenu {...this.props.appProps}/>
 				<Child {...this.props} />
 				<h4>This is the footer</h4>
 			</div>
