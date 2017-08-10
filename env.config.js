402be1d727121aa9dacdd0350707f98f56bcb5c6
Paulo Barcelos <paulo@azucrina.org>
2017-08-10 15:41:10 +0200

    Preparing for now deploy

--- env.config.js
+++ env.config.js
@@ -1,5 +1,10 @@
+const GRAPHQL_PORT = process.env.GRAPHQL_PORT || 5000
+const CACHE_PORT = process.env.CACHE_PORT || 4000
 module.exports = {
+	// eslint-disable-next-line no-unneeded-ternary
 	STATIC         : process.env.STATIC ? true : false,
-	CACHE_SERVER   : 'http://localhost:4000',
-	GRAPHQL_SERVER : 'https://strawbees-content-graphql-server-lsrcwnbjfi.now.sh'
+	CACHE_SERVER   : `http://127.0.0.1:${CACHE_PORT}`,
+	GRAPHQL_SERVER : `http://127.0.0.1:${GRAPHQL_PORT}`,
+	GRAPHQL_PORT,
+	CACHE_PORT
 }
