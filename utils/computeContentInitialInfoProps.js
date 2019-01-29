e487932e1f22bbab5ce590fb76d97dca53db3208
Paulo Barcelos <paulo@azucrina.org>
2019-01-29 17:33:27 +0100

    Avoiding throwing errors in case the lesson plan has missing information

--- utils/computeContentInitialInfoProps.js
+++ utils/computeContentInitialInfoProps.js
@@ -1,13 +1,13 @@
 export default (props, appProps) => ({
 	initialInfo : {
-		duration       : props.duration.title,
-		durationLabel  : appProps.strings.duration,
-		groupSize      : props.groupSize.title,
-		groupSizeLabel : appProps.strings.groupSize,
-		classSize      : props.classSize.title,
-		classSizeLabel : appProps.strings.classSize,
-		overview       : props.overview,
-		overviewLabel  : appProps.strings.overview,
-		gallery        : props.gallery
+		duration       : props && props.duration && props.duration.title,
+		durationLabel  : appProps && appProps.strings && appProps.strings.duration,
+		groupSize      : props && props.groupSize && props.groupSize.title,
+		groupSizeLabel : appProps && appProps.strings && appProps.strings.groupSize,
+		classSize      : props && props.classSize && props.classSize.title,
+		classSizeLabel : appProps && appProps.strings && appProps.strings.classSize,
+		overview       : props && props.overview,
+		overviewLabel  : appProps && appProps.strings && appProps.strings.overview,
+		gallery        : props && props.gallery
 	}
 })
